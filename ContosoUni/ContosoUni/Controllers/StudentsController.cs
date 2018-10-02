59a2fccd1ab8bdb4bb84845d23e1a5d1a6afa6fc
Dark-wiz <somatic20@yahoo.com>
2018-10-02 14:22:21 +0100

    update create action method in Student controller

--- ContosoUni/ContosoUni/Controllers/StudentsController.cs
+++ ContosoUni/ContosoUni/Controllers/StudentsController.cs
@@ -35,7 +35,7 @@ namespace ContosoUni.Controllers
 
             var student = await _context.Students
                 .Include(s => s.Enrollments)
-                    .ThenInclude(e => e.Course)
+                .ThenInclude(e => e.Course)
                 .AsNoTracking()
                 .SingleOrDefaultAsync(m => m.ID == id);
                 //.FirstOrDefaultAsync(m => m.ID == id);
@@ -58,13 +58,23 @@ namespace ContosoUni.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,LastName,FirstMidName,EnrollmentDate")] Student student)
+        public async Task<IActionResult> Create([Bind("LastName,FirstMidName,EnrollmentDate")] Student student)
         {
-            if (ModelState.IsValid)
+            try
             {
-                _context.Add(student);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                if (ModelState.IsValid)
+                {
+                    _context.Add(student);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (DbUpdateException /* ex */)
+            {
+                //Log the error (uncomment ex variable name and write a log.
+                ModelState.AddModelError("", "Unable to save changes. " +
+                                             "Try again, and if the problem persists " +
+                                             "see your system administrator.");
             }
             return View(student);
         }
