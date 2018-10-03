95816a9ad0ed04a0143da7d8e0d479830ca5106f
Dark-wiz <somatic20@yahoo.com>
2018-10-03 20:15:46 +0100

    Update Student model

--- ContosoUni/ContosoUni/Models/Student.cs
+++ ContosoUni/ContosoUni/Models/Student.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,15 +11,24 @@ namespace ContosoUni.Models
     {
         public int ID { get; set; }
         [StringLength(50)]
-        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Required]
+        //[RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Display(Name = "Last Name")]
         public string LastName { get; set;}
-        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        //[RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Required]
         [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters")]
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
         public string FirstMidName { get; set; }
         //
+        [Display(Name = "Enrollment Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
+        [Display(Name = "Full Name")]
+        public string FullName {
+            get { return LastName + " " + FirstMidName; } }
         public ICollection<Enrollment> Enrollments { get; set; }
     }
 }
