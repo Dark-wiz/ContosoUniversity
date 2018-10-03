c7993edfc291208a783cc0f3345ec87685bc76bd
Dark-wiz <somatic20@yahoo.com>
2018-10-03 21:33:07 +0100

    Added CourseAssignment, OfficeAssignment and Instructor models. Edited Enrollments and Course models

--- ContosoUni/ContosoUni/Data/SchoolContext.cs
+++ ContosoUni/ContosoUni/Data/SchoolContext.cs
@@ -17,12 +17,22 @@ namespace ContosoUni.Data
         public DbSet<Course> Courses{ get; set; }
         public DbSet<Enrollment> Enrollments{ get; set; }
         public DbSet<Student> Students{ get; set; }
+        public DbSet<Department> Departments{ get; set; }
+        public DbSet<Instructor> Instructors{ get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignments{ get; set; }
+        public DbSet<CourseAssignment> CourseAssignments { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Course>().ToTable("Course");
             modelBuilder.Entity<Student>().ToTable("Student");
             modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Department>().ToTable("Department");
+            modelBuilder.Entity<Instructor>().ToTable("OfficeAssignment");
+            modelBuilder.Entity<OfficeAssignment>().ToTable("Enrollment");
+            modelBuilder.Entity<CourseAssignment>().ToTable("CourseAssignment");
+
+            modelBuilder.Entity<CourseAssignment>().HasKey(c => new {c.CourseID, c.InstructorID});
         }
         //create db connection without the need of context constructor
         protected override void OnConfiguring(DbContextOptionsBuilder builder)
