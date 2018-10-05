8a4408345a400da180403bbd75c432b13579fac2
Dark-wiz <somatic20@yahoo.com>
2018-10-05 12:57:39 +0100

    Add Person entity and use it as a base class to implement Inheritance. Both Instructor and Student entity are sub-classes to Person entity

--- ContosoUni/ContosoUni/Models/Student.cs
+++ ContosoUni/ContosoUni/Models/Student.cs
@@ -7,28 +7,13 @@ using System.Threading.Tasks;
 
 namespace ContosoUni.Models
 {
-    public class Student
+    public class Student : Person
     {
-        public int ID { get; set; }
-        [StringLength(50)]
-        [Required]
-        //[RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
-        [Display(Name = "Last Name")]
-        public string LastName { get; set;}
-        //[RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
-        [Required]
-        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters")]
-        [Column("FirstMidName")]
-        [Display(Name = "First Name")]
-        public string FirstMidName { get; set; }
         //
         [Display(Name = "Enrollment Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
-        [Display(Name = "Full Name")]
-        public string FullName {
-            get { return LastName + " " + FirstMidName; } }
         public ICollection<Enrollment> Enrollments { get; set; }
     }
 }
